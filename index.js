74a5885be34c2d0fe7326dbd49ca7c4c76073555
luis.poveda93 <luis.poveda9321@gmail.com>
2022-11-28 22:25:09 +0100

    callback url fixed

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ app.get('/api/create-paper-intent', (req, res) => {
             contractArgs: {tokenId: `${listingId}`},
             imageUrl: `${img}`,
             description: 'Please, choose the quantity',
-            successCallbackUrl: 'https://bookverse.vercel.app/store',
-            cancelCallbackUrl: `https://bookverse.vercel.app/store/${listingId}`
+            successCallbackUrl: 'https://bookverse.vercel.app/',
+            cancelCallbackUrl: `https://bookverse.vercel.app/${listingId}`
         }
     };
 
