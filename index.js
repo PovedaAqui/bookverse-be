0c27d305d7f31c7fa5aca852d6f9e2bb9f390cd7
luis.poveda93 <luis.poveda9321@gmail.com>
2022-12-07 23:17:57 +0100

    fixing bug

--- index.js
+++ index.js
@@ -67,30 +67,27 @@ app.get('/api/get-my-books', async (req, res) => {
 
   try {
     client.get(address, async (err, data) => {
-      if (err) console.log(err);
+      if (err) throw err;
+
       if (data !== null) {
         res.json(JSON.parse(data));
       } else {
         const options = {
-          method: 'GET',
-          url: `https://api.nftport.xyz/v0/accounts/${address}`,
-          params: {
-          chain: chain,
-          include: 'metadata',
-          contract_address: process.env.REACT_APP_DROP_CONTRACT
-          },
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: process.env.REACT_APP_NFT_PORT
-            }
+        method: 'GET',
+        url: `https://api.nftport.xyz/v0/accounts/${address}`,
+        params: {
+        chain: chain,
+        include: 'metadata',
+        contract_address: process.env.REACT_APP_DROP_CONTRACT
+        },
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: process.env.REACT_APP_NFT_PORT
+          }
         };
-        try {
           const response = await axios.request(options);
           client.setex(address, 3600, JSON.stringify(response.data)); // Store data in Redis cache
           res.json(response.data);
-        } catch (error) {
-          console.error(error);
-        }
     }});
   } catch (error) {
     console.error(error);
